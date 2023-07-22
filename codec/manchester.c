2cee67785bbc530ad8bd74f08f6821973ba3720f
Anti <89676899+UncleAnti@users.noreply.github.com>
2023-07-22 14:25:42 +1200

    moved bitcount to only count binary values didnt like the bitshift multiply/divide
    Signed-off-by: Anti <89676899+UncleAnti@users.noreply.github.com>

--- codec/manchester.c
+++ codec/manchester.c
@@ -9,7 +9,7 @@ void man_decode(FILE *in, FILE *out, const char * opt){
 	uint8_t last = 0;
 
   char * in_data  = malloc(BIT_BUF_SIZE);
-  char * out_data = malloc(BIT_BUF_SIZE >> 1); // 1/2 the size
+  char * out_data = malloc(BIT_BUF_SIZE / 2); // 1/2 the size
 
 	size_t out_pos = 0;
 	size_t ret = 0;
@@ -17,8 +17,8 @@ void man_decode(FILE *in, FILE *out, const char * opt){
 		for(size_t idx = 0; idx < ret; ++idx){
 			const uint8_t byte = in_data[idx];
 			if (byte == '0' || byte == '1'){
-				bitcount++;
 				uint8_t src = (byte == '1');
+
 				if (!init) {
 					init = 1;
 					last = src;
@@ -27,23 +27,28 @@ void man_decode(FILE *in, FILE *out, const char * opt){
 
 				init = 0;
 				if (src ^ last) {
+					bitcount++;
 					if (src)
 						out_data[out_pos++] = '0';
 					else
 						out_data[out_pos++] = '1';
-				}
-				if (bitcount == 8 && space) {
-					out_data[out_pos++] = ' ';
-					bitcount = 0;
+
+					if (bitcount == 8 && space) {
+						out_data[out_pos++] = ' ';
+						bitcount = 0;
+					}
 				}
 			}
 		}
+
 		fwrite(out_data, sizeof(char), out_pos, out);
 		out_pos = 0;
 	}
-	fflush(out);
+
 	free(out_data);
 	free(in_data);
+
+	fflush(out);
 }
 
 void man_encode(FILE *in, FILE *out, const char * opt){
@@ -51,38 +56,43 @@ void man_encode(FILE *in, FILE *out, const char * opt){
 	int bitcount = 0;
 
   char * in_data  = malloc(BIT_BUF_SIZE);
-  char * out_data = malloc(BIT_BUF_SIZE << 1); // twice the size
+  char * out_data = malloc(BIT_BUF_SIZE * 2);
 
 	size_t out_pos = 0;
 	size_t ret = 0;
   while ((ret = fread(in_data, sizeof(char), BIT_BUF_SIZE, in)) > 0){
 		for(size_t idx = 0; idx < ret; ++idx){
 			const uint8_t byte = in_data[idx];
-			bitcount++;
 			switch(byte){
 			case '0':
 				out_data[out_pos++] = '0';
 				out_data[out_pos++] = '1';
+				bitcount++;
 				break;
 			case '1':
 				out_data[out_pos++] = '1';
 				out_data[out_pos++] = '0';
+				bitcount++;
 				break;
 			default:
 				out_data[out_pos++] = byte;
 			}
+
 			if (bitcount == 8 && space){
 				out_data[out_pos++] = ' ';
 				bitcount = 0;
 
 			}
 		}
+
 		fwrite(out_data, sizeof(char), out_pos, out);
 		out_pos = 0;
 	}
-	fflush(out);
+
 	free(out_data);
 	free(in_data);
+
+	fflush(out);
 }
 
 ENGINE_EXPORT(
